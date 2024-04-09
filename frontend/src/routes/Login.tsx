93cd640f483fe9ac781299cb20cc9f693aecbfaa
Mithun Munsi <mithun.munsi@gmail.com>
2024-04-09 11:19:51 +0300

    register page added

---
+++ frontend/src/routes/Login.tsx
@@ -0,0 +1,12 @@
+// styles
+import './Register.css';
+
+const Login: React.FC = () => {
+  return (
+    <div className="register">
+      <h2>Login</h2>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/routes/Register.tsx
+++ frontend/src/routes/Register.tsx
@@ -1,10 +1,30 @@
 // styles
-import "./Register.css";
+import './Register.css';
 
 const Register: React.FC = () => {
   return (
     <div className="register">
-      <h2>Register</h2>
+      <h2>Create Your Account Here</h2>
+      <div className="register__group">
+        <label htmlFor="">Name:</label>
+        <input type="text" placeholder="Type Your Name Here!" required />
+      </div>
+      <div className="register__group">
+        <label htmlFor="">Email:</label>
+        <input type="text" placeholder="Enter Your Email Address!" required />
+      </div>
+      <div className="register__group">
+        <label htmlFor="">Password:</label>
+        <input type="password" placeholder="Set Your Password" required />
+      </div>
+      <div className="register__group">
+        <label htmlFor="">Confirm Password:</label>
+        <input type="password" placeholder="Confirm Your Passwowrd" required />
+      </div>
+      <button className="register__btn">Register</button>
+      <p>
+        Already Have An Account? <button className="login__btn">Login</button>
+      </p>
     </div>
   );
 };
